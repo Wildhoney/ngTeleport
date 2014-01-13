706fe4d3f8dafabce54f76c5e9f806d98ff7ba31
Adam Timberlake <adam.timberlake@gmail.com>
2014-01-13 14:09:38 +0000

    Added example syntax

--- example/js/application.js
+++ example/js/application.js
@@ -2,4 +2,49 @@
 
     var app = $angular.module('exampleApp', []);
 
+    /**
+     * @controller FirstController
+     */
+    app.controller('FirstController', function($scope) {
+
+        $scope.moveAcross = function moveAcross(element) {
+            alert(element);
+        }
+
+    });
+
+    /**
+     * @controller SecondController
+     */
+    app.controller('SecondController', function() {
+
+    });
+
+    /**
+     * @directive button
+     */
+    app.directive('button', function move($window) {
+
+        return {
+            restrict: 'E',
+
+            link: function link(scope, element, attributes) {
+
+                element.bind('click', function onClick() {
+
+                    var sourceContainer = element.parent(),
+                        targetContainer = angular.element($window.document.querySelector('.second'));
+
+                    teleport({
+                        from: sourceContainer,
+                        to: targetContainer
+                    });
+
+                });
+
+            }
+        }
+
+    });
+
 })(window.angular);
