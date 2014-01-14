b899b88ce7bed2a3a55fd75fe19a72af87cc12da
Adam Timberlake <adam.timberlake@gmail.com>
2014-01-14 22:13:08 +0000

    Checking ngTeleport attr. Fixes #2

--- package/Teleport.js
+++ package/Teleport.js
@@ -59,9 +59,20 @@
             // Determine whether we should inherit the scope or not.
             var inheritScope = (options && options.retainScope);
 
+            // Determine which scope to use for the teleported item, and grab the `ng-teleport` attribute.
             var scope        = (inheritScope) ? sourceContainer.scope() : targetContainer.scope(),
-                reference    = sourceContainer.attr('ng-teleport'),
-                html         = _teleportable[reference + sourceContainer.scope().$id],
+                reference    = sourceContainer.attr('ng-teleport');
+
+            if (!reference) {
+
+                // Notify the developer they're trying to teleport a non-teleportable node.
+                throw 'ngTeleport: Attempting to teleport a node without `ng-teleport` attribute.';
+
+            }
+
+            // Retrieve the template HTML from our object and interpolate then compile it based on
+            // the scope determined above.
+            var html         = _teleportable[reference + sourceContainer.scope().$id],
                 interpolated = $interpolate(html)(scope),
                 element      = angular.element(interpolated),
                 compiled     = $compile(element)(scope);
@@ -70,9 +81,13 @@
             _teleportable[reference + scope.$id] = html;
 
             if (!options || (options.duplicate || false) === false) {
+
+                // Remove the container that was teleported if we're instructed to do so.
                 sourceContainer.remove();
+
             }
 
+            // Finally we can teleport the node based on the insertion method chosen.
             targetContainer[options && options.insertion || 'append'](compiled);
 
         };
